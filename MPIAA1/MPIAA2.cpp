c40c302ecb89de7b3e023881c2950546f9d731b5
Тривайлов Виталий <betmen201446@gmail.com>
2024-09-26 04:05:44 +0700

    2.1

--- MPIAA1/MPIAA2.cpp
+++ MPIAA1/MPIAA2.cpp
@@ -1,15 +1,17 @@
 ﻿// MPIAA2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
+#include <ctime> 
 #include <omp.h>
 #include <fstream>
 #include <iostream>
+#include <chrono>
 
 using namespace std;
 
 int main()
 {
    
-   int n = 1000000;
+   int n = 10;
    double s = 0;
 
    double* x = NULL; // объявление
@@ -30,13 +32,22 @@ int main()
       inputFile2 >> y[i];
    inputFile2.close();
 
+   auto begin = std::chrono::steady_clock::now();
+
    float start_time = clock();
-//#pragma omp parallel for num_threads(2) reduction(+:s)
+#pragma omp parallel for num_threads(16) reduction(+:s)
       for (int i = 0; i < n; i++)
          s += x[i] * y[i];
  
    float end_time = clock();
    float search_time = end_time - start_time;
+
+   auto end = chrono::steady_clock::now();
+
+   auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin); cout << "The time: " << elapsed_ms.count() << " ms\n";
+   cout << endl;
+
+   cout << search_time / 1000 << " s\n";
 
    cout << sqrt(s)<<"\n";
    cout << sqrt(search_time);
